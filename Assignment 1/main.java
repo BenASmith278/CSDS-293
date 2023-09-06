f6db8464f89f24f653dede8e193cd9295a7f31a1
Ben Smith <benasmith278@gmail.com>
2023-09-06 11:36:33 -0400

    IDK Java

---
+++ Assignment 1/main.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.ListIterator;
+
+class Assignment1 {
+    static <T extends Comparable<? super T>> List<T> longestHigherSuffix(List<T> a, List<T> b, Comparator<T> cmp) {
+        List<T> result = new ArrayList<T>();
+        ListIterator<T> aIter = a.listIterator();
+        ListIterator<T> bIter = b.listIterator();
+        
+        return a;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
