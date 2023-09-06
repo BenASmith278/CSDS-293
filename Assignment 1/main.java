5d7010c481e137c93c67495736535762592343c7
Ben Smith <benasmith278@gmail.com>
2023-09-06 15:04:37 -0400

    Done

--- Assignment 1/main.java
+++ Assignment 1/main.java
@@ -1,18 +1,22 @@
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.*;
 
-class Assignment1 {
+class AssignmentOne {
     static <T extends Comparable<? super T>> List<T> longestHigherSuffix(List<T> a, List<T> b, Comparator<T> cmp) {
         List<T> result = new ArrayList<T>();
-        ListIterator<T> aIter = a.listIterator();
-        ListIterator<T> bIter = b.listIterator();
-        
-        return a;
-    }
+        ListIterator<T> aIter = a.listIterator(a.size());
+        ListIterator<T> bIter = b.listIterator(b.size());
 
-    public static void main(String[] args) {
-        
+        while (aIter.hasPrevious() && bIter.hasPrevious()) {
+            T aVal = aIter.previous();
+            T bVal = bIter.previous();
+
+            if (cmp.compare(aVal, bVal) >= 0) {
+                result.add(0, aVal);
+            } else {
+                break;
+            }
+        }
+
+        return result;
     }
 }
