d1ef9c802bbf481ee48139bccece16c684cd2274
Ben Smith <benasmith278@gmail.com>
2023-09-19 14:10:06 -0400

    Double Ring Tests

---
+++ ring/ring/DoubleRingTests.java
@@ -0,0 +1,33 @@
+package ring;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+class DoubleRingTests {
+    Ring<Double> ring = new DoubleRing();
+
+    @Test
+    void sum() {
+        assertEquals(new Double(2.5), ring.sum(1.0, 1.5));
+        assertEquals(new Double(-2.5), ring.sum(0.5, -3.0));
+    }
+
+    @Test
+    void product() {
+        assertEquals(new Double(9.0), ring.product(4.5, 2.0));
+        assertEquals(new Double(-7), ring.product(2.0, -3.0));
+        assertEquals(new Double(0.0), ring.product(0.0, 1.5));
+        assertEquals(new Double(7.0), ring.product(-2.0, -3.5));
+    }
+
+    @Test
+    void zero() {
+        assertEquals(new Double(0.0), ring.zero());
+    }
+
+    @Test
+    void identity() {
+        assertEquals(new Double(1.0), ring.identity());
+    }
+}
