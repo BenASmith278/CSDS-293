ff1ba381e25a2b97a6f11c1ecf7a38f8d7f43d42
Ben Smith <benasmith278@gmail.com>
2023-09-19 14:13:25 -0400

    Fix Tests

--- ring/ring/DoubleRingTests.java
+++ ring/ring/DoubleRingTests.java
@@ -4,30 +4,32 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-class DoubleRingTests {
+public class DoubleRingTests {
     Ring<Double> ring = new DoubleRing();
 
     @Test
-    void sum() {
+    public void sum() {
         assertEquals(new Double(2.5), ring.sum(1.0, 1.5));
         assertEquals(new Double(-2.5), ring.sum(0.5, -3.0));
+        assertThrows(NullPointerException.class, () -> ring.sum(null, 2.5));
     }
 
     @Test
-    void product() {
+    public void product() {
         assertEquals(new Double(9.0), ring.product(4.5, 2.0));
-        assertEquals(new Double(-7), ring.product(2.0, -3.0));
+        assertEquals(new Double(-7), ring.product(2.0, -3.5));
         assertEquals(new Double(0.0), ring.product(0.0, 1.5));
         assertEquals(new Double(7.0), ring.product(-2.0, -3.5));
+        assertThrows(NullPointerException.class, () -> ring.product(null, 2.5));
     }
 
     @Test
-    void zero() {
+    public void zero() {
         assertEquals(new Double(0.0), ring.zero());
     }
 
     @Test
-    void identity() {
+    public void identity() {
         assertEquals(new Double(1.0), ring.identity());
     }
 }
--- ring/ring/IntegerRingTests.java
+++ ring/ring/IntegerRingTests.java
@@ -4,18 +4,18 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-class IntegerRingTests {
+public class IntegerRingTests {
     Ring<Integer> ring = new IntegerRing();
 
     @Test
-    void sum() {
+    public void sum() {
         assertEquals(new Integer(3), ring.sum(1, 2));
         assertEquals(new Integer(-2), ring.sum(1, -3));
         assertEquals(Integer.MIN_VALUE, ring.sum(Integer.MAX_VALUE, 1).intValue());
     }
 
     @Test
-    void product() {
+    public void product() {
         assertEquals(new Integer(8), ring.product(4, 2));
         assertEquals(new Integer(-6), ring.product(2, -3));
         assertEquals(new Integer(0), ring.product(0, 1));
@@ -23,12 +23,12 @@ class IntegerRingTests {
     }
 
     @Test
-    void zero() {
+    public void zero() {
         assertEquals(new Integer(0), ring.zero());
     }
 
     @Test
-    void identity() {
+    public void identity() {
         assertEquals(new Integer(1), ring.identity());
     }
 }
