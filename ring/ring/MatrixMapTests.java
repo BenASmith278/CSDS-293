b1b2a7479850e22134c505681f37edd82efc2ef4
Ben Smith <benasmith278@gmail.com>
2023-10-06 17:30:10 -0400

    Set up new tests

--- ring/ring/MatrixMapTests.java
+++ ring/ring/MatrixMapTests.java
@@ -2,28 +2,43 @@ package ring;
 
 import static org.junit.Assert.*;
 
+import java.math.BigInteger;
+
 import org.junit.Test;
 
+import ring.MatrixMap.InconsistentSizeException;
 import ring.MatrixMap.InvalidLengthException;
+import ring.MatrixMap.NonSquareException;
 import ring.MatrixMap.InvalidLengthException.Cause;
 
 public class MatrixMapTests {
-    Indexes zeroIndex = new Indexes(0, 0);
-    Indexes oneIndex = new Indexes(1, 1);
-    Indexes nonZeroIndex = new Indexes(1, 2);
-    Indexes nullIndex = null;
-    Integer[][] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
-    Integer[][] onesMatrix = { { 1, 1, 1 }, { 1, 1, 1 }, { 1, 1, 1 } };
-    Integer[][] identityMatrix = { { 1, 0, 0, 0, 0 }, { 0, 1, 0, 0, 0 }, { 0, 0, 1, 0, 0 }, { 0, 0, 0, 1, 0 },
+    public Indexes zeroIndex = new Indexes(0, 0);
+    public Indexes oneIndex = new Indexes(1, 1);
+    public Indexes nonZeroIndex = new Indexes(1, 2);
+    public Indexes nullIndex = null;
+    public Integer[][] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+    public Integer[][] onesMatrix = { { 1, 1, 1 }, { 1, 1, 1 }, { 1, 1, 1 } };
+    public Integer[][] identityMatrix = { { 1, 0, 0, 0, 0 }, { 0, 1, 0, 0, 0 }, { 0, 0, 1, 0, 0 }, { 0, 0, 0, 1, 0 },
             { 0, 0, 0, 0, 1 } };
-    Integer[][] functionMatrix = { { 0, 1, 2 }, { 1, 2, 3 }, { 2, 3, 4 } };
-    Integer[][] emptyMatrix = {};
-    Integer[][] nullMatrix = null;
-    MatrixMap<Integer> matrixMap = MatrixMap.from(matrix);
-    MatrixMap<Integer> matrixMapConstant = MatrixMap.constant(2, 1);
-    MatrixMap<Integer> matrixMapIdentity = MatrixMap.identity(4, 0, 1);
-    MatrixMap<Integer> matrixMapNull = null;
-    InvalidLengthException exception = new InvalidLengthException(InvalidLengthException.Cause.ROW, 0);
+    public Integer[][] functionMatrix = { { 0, 1, 2 }, { 1, 2, 3 }, { 2, 3, 4 } };
+    public Integer[][] emptyMatrix = {};
+    public Integer[][] nullMatrix = null;
+    public BigInteger[][] bigMatrix = { { BigInteger.ZERO, BigInteger.ONE, BigInteger.TWO },
+            { BigInteger.ONE, BigInteger.TWO, BigInteger.valueOf(3) } };
+    public MatrixMap<Integer> matrixMap = MatrixMap.from(matrix);
+    public MatrixMap<Integer> matrixMapConstant = MatrixMap.constant(2, 1);
+    public MatrixMap<Integer> matrixMapIdentity = MatrixMap.identity(4, 0, 1);
+    public MatrixMap<Integer> matrixMapNull = null;
+    public MatrixMap<BigInteger> bigMatrixMap = MatrixMap.instance(new Indexes(4, 4),
+            indexes -> BigInteger.valueOf(indexes.row() + indexes.column()));
+    public MatrixMap<BigInteger> bigMatrixMapIdentity = MatrixMap.identity(4, BigInteger.ZERO, BigInteger.ONE);
+    public MatrixMap<BigInteger> bigMatrixMapConstant = MatrixMap.constant(2, BigInteger.TWO);
+    public MatrixMap<BigInteger> bigMatrixMapRectangular = MatrixMap.from(bigMatrix);
+    public MatrixMap<BigInteger> bigMatrixMapRectangularTwo = MatrixMap.from(bigMatrix);
+    public MatrixMap<BigInteger> bigMatrixMapNull = null;
+    public InvalidLengthException invalidLength = new InvalidLengthException(InvalidLengthException.Cause.ROW, 0);
+    public InconsistentSizeException inconsistentSize = new InconsistentSizeException(oneIndex, nonZeroIndex);
+    public NonSquareException nonSquare = new NonSquareException(nonZeroIndex);
 
     public Integer testFunction(Indexes indexes) {
         return indexes.row() + indexes.column();
@@ -63,9 +78,10 @@ public class MatrixMapTests {
 
     @Test
     public void exceptions() {
-        assertEquals(Cause.ROW, exception.cause());
-        assertEquals(0, exception.length());
+        assertEquals(Cause.ROW, invalidLength.cause());
+        assertEquals(0, invalidLength.length());
         assertThrows(IllegalArgumentException.class, () -> InvalidLengthException.requireNonEmpty(Cause.ROW, 0));
+
     }
 
     @Test
@@ -103,4 +119,11 @@ public class MatrixMapTests {
                 MatrixMap.from(functionMatrix));
     }
 
+    @Test
+    public void plus() {
+    }
+
+    @Test
+    public void times() {
+    }
 }
